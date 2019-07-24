6a8e8c2f4341b3a284b2d277b8c2fd3c12b296a7
LucasAngeloni <38081754+LucasAngeloni@users.noreply.github.com>
2019-07-23 21:54:38 -0300

    Update ejercicio_01.py

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -5,14 +5,15 @@
 # - apellido: string (longitud 250)
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
 
 Base = declarative_base()
-
 
 class Socio(Base):
     __tablename__ = 'socios'
 
-    # id = Column(...)
-    # dni = Column(...)
-    # nombre = Column(...)
-    # apellido = Column(...)
+    id_socio = Column(Integer,primary_key=True)
+    dni = Column(Integer,unique=True)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
